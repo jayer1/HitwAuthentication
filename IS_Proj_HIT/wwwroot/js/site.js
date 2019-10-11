66d1e36f5756fe8af0ce1409ed01360485c3ad73
jayer1 <jayer1@my.wctc.edu>
2019-10-10 23:11:28 -0500

    Added list of patients page, edit and delete patients function + added validation to edit page

--- IS_Proj_HIT/wwwroot/js/site.js
+++ IS_Proj_HIT/wwwroot/js/site.js
@@ -93,6 +93,69 @@ $(function(){
         }
     });
 
+    $("form[name='patientEdit']").validate({
+        // Specify validation rules
+        rules: {
+            Mrn: {
+                required: true,
+                minlength: 6
+            },
+            FirstName: {
+                required: true,
+                alphabetsnspace: true
+            },
+            Middle: "alphabetsnspace",
+            lastName: {
+                required: true,
+                alphabetsnspace: true
+            },
+            aliasFirstName: "alphabetsnspace",
+            aliasMiddleName: "alphabetsnspace",
+            aliasLastName: "alphabetsnspace",
+            MaidenName: "alphabetsnspace",
+            MothersMaidenName: "alphabetsnspace",
+            Dob: {
+                required: true,
+                date: true,
+                maxDate: true,
+                minDate: true
+            },
+            Ssn: {
+                required: true,
+                minlength: 9,
+                number: true
+            }
+
+        },
+        // Specify validation error messages
+        messages: {
+            FirstName: {
+                required: "Please provide a first name",
+                alphabetsnspace: "First name must be all letters"
+            },
+            lastName: {
+                required: "Please provide a last name",
+                alphabetsnspace: "Last name must be all letters"
+            },
+            Ssn: {
+                required: "Please provide a Social Security Number",
+                minlength: "Your SSN must be at least 10 digits long",
+                number: "SSN should be numeric"
+            },
+            Middle: "Only one letter allowed",
+            aliasFirstName: "Only letters allowed",
+            aliasMiddleName: "Only letters allowed",
+            aliasLastName: "Only letters allowed",
+            MothersMaidenName: "Only letters allowed",
+            MaidenName: "Only letters allowed"
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
     $("form[name='employment']").validate({
         // Specify validation rules
         rules: {
